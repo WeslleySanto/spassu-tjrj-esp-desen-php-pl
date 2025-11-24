3d3a082c03ea2238b8a7c20930d86947a9bfd798
Weslley Santo <weslley.santo@gmail.com>
2025-11-24 16:08:11 -0300

    create AutorService class for managing Autor operations

---
+++ app/Services/AutorService.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Autor;
+use App\Repositories\AutorRepository;
+use Illuminate\Database\Eloquent\Collection as CollectionDatabase;
+
+class AutorService
+{
+    /**
+     * AutorService constructor.
+     *
+     * @param AutorRepository $autorRepository
+     */
+    public function __construct(
+        private AutorRepository $autorRepository,
+    ) {}
+
+    /**
+     * Get all Autor
+     *
+     * @return CollectionDatabase
+     */
+    public function index(): CollectionDatabase
+    {
+        return $this->autorRepository->index();
+    }
+
+    /**
+     * Create a new Autor
+     *
+     * @param array $data
+     * @return void
+     */
+    public function store(array $data): void
+    {
+        $this->autorRepository->create($data);
+    }
+
+    /**
+     * Find an Autor by its ID or fail
+     *
+     * @param integer $id
+     * @return Autor
+     */
+    public function findOrFail(int $id): Autor
+    {
+        return $this->autorRepository->findOrFail($id);
+    }
+
+    /**
+     * Delete an Autor by its ID
+     *
+     * @param integer $id
+     * @return void
+     */
+    public function destroy(int $id): void
+    {
+        $this->autorRepository->destroy($id);
+    }
+}

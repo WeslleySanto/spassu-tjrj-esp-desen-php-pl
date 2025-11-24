95cb6efb3de4b9144059ef74b9b71952254da5d0
Weslley Santo <weslley.santo@gmail.com>
2025-11-24 15:11:06 -0300

    create Autor model with relationships to livros

---
+++ app/Models/Autor.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Autor extends Model
+{
+    protected $table = 'autores';
+    protected $primaryKey = 'CodAu';
+    public $timestamps = false;
+
+    protected $fillable = [
+        'CodAu', 'Nome'
+    ];
+
+    public function livros()
+    {
+        return $this->belongsToMany(Livro::class, 'livro_autor', 'Autor_CodAu', 'Livro_Codl');
+    }
+}

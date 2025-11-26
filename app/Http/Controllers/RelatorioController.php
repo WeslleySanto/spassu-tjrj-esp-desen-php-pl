4b228fc2be7201a84b0e9fa4b2b9da9448a9955e
Weslley Santo <weslley.santo@gmail.com>
2025-11-26 15:27:19 -0300

    add RelatorioController to handle report generation and PDF export

---
+++ app/Http/Controllers/RelatorioController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Dompdf\Dompdf;
+use Illuminate\View\View;
+use App\Services\RelatorioService;
+use Symfony\Component\HttpFoundation\StreamedResponse;
+
+class RelatorioController extends Controller
+{
+    /**
+     * RelatorioController constructor.
+     *
+     * @param RelatorioService $relatorioService
+     */
+    public function __construct(
+        private RelatorioService $relatorioService
+    ) {}
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return View
+     */
+    public function index(): View
+    {
+        $dados = $this->relatorioService->gerarRelatorio();
+
+        return view('relatorios.livros', compact('dados'));
+    }
+
+    /**
+     * Generate PDF report
+     *
+     * @return  \Symfony\Component\HttpFoundation\StreamedResponse
+     */
+    public function pdf(): StreamedResponse
+    {
+        $dados = $this->relatorioService->gerarRelatorio();
+
+        $html = view('relatorios.livros', compact('dados'))->render();
+
+        $pdf = new Dompdf();
+        $pdf->loadHtml($html);
+        $pdf->setPaper('A4', 'portrait');
+        $pdf->render();
+
+        return $pdf->stream("relatorio_livros_por_autor.pdf");
+    }
+}

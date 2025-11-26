fa4a9c6d066bbd6f3d76286b57c1f302d1ea9dac
Weslley Santo <weslley.santo@gmail.com>
2025-11-26 15:27:27 -0300

    add routes for report generation and PDF export

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AutorController;
 use App\Http\Controllers\LivroController;
 use App\Http\Controllers\AssuntoController;
+use App\Http\Controllers\RelatorioController;
 
 Route::get('/', function () {
     return redirect()->route('livros.index');
@@ -12,3 +13,5 @@ Route::get('/', function () {
 Route::resource('livros', LivroController::class);
 Route::resource('autores', AutorController::class);
 Route::resource('assuntos', AssuntoController::class);
+Route::get('/relatorio/livros', [RelatorioController::class, 'index'])->name('relatorio.livros');
+Route::get('/relatorio/pdf', [RelatorioController::class, 'pdf'])->name('relatorio.pdf');
